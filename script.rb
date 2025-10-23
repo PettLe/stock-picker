69557389d9ca3330dc055d772d350fa318f15ffd
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-23 14:37:47 +0300

    Able to determine best day to buy and sell

--- script.rb
+++ script.rb
@@ -1,13 +1,13 @@
 def stock_picker(array)
+  all_results = Array.new()
   array.map do |number|
-    p "Uusi kierros alkaa, nyt lähtee nro #{number}"
     array.map do |number2|
       if array.find_index(number2) > array.find_index(number)
-        p number2
+        all_results.push({:arr => [array.find_index(number), array.find_index(number2)], :profit => number2 - number})
       end
     end
-    #p array.find_index(number)
   end
+  p "#{all_results.max_by {|item| item[:profit]}[:arr]} for a profit of $#{array[all_results.max_by {|item| item[:profit]}[:arr][1]]} - $#{array[all_results.max_by {|item| item[:profit]}[:arr][0]]} == $#{all_results.max_by {|item| item[:profit]}[:profit]}"
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
